080f890827e4a191a9deeabc347045f81b664607
Vetési Zoltán <vetesi.zoltan@gmail.com>
2017-11-17 19:05:30 +0100

    fix test on windows

--- tests/test_file.py
+++ tests/test_file.py
@@ -4,12 +4,20 @@ from io import StringIO
 from zibo import json as zibo_json
 
 
-@pytest.mark.parametrize("value,expected", [
-    ({"Hello": "World"}, '{"Hello":"World"}'),
-    ({"Hello": "Árvíztűrő tükörfúrógép"}, py_json.dumps),
-    ("Под водом проводе скоро половину свог живота", py_json.dumps),
-    ("Под водом проводе скоро половину свог живота" * 100000, py_json.dumps),
-])
+@pytest.mark.parametrize(
+    "value,expected",
+    [
+        ({"Hello": "World"}, '{"Hello":"World"}'),
+        ({"Hello": "Árvíztűrő tükörfúrógép"}, py_json.dumps),
+        ("Под водом проводе скоро половину свог живота", py_json.dumps),
+        ("Под водом проводе скоро половину свог живота" * 100000, py_json.dumps),
+    ],
+    ids=[
+        "Hello World",
+        "Árvíztűrő tükörfúrógép",
+        "Short UTF-8",
+        "Long UTF-8"
+    ])
 def test_encode_to_file(value, expected, ensure_ascii):
     with StringIO() as s:
         zibo_json.dump(value, s, ensure_ascii=ensure_ascii)
