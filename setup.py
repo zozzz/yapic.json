ea45cb96dbcb0c7e37329d3739ab24256c101a47
Vetési Zoltán <zozzz@trigon.hu>
2022-10-03 19:41:39 +0200

    chore(bump): 1.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, Extension
 from setuptools.command.test import test as TestCommand
 from setuptools import Command
 
-VERSION = "1.7.3"
+VERSION = "1.8.0"
 
 define_macros = {
     "YAPIC_JSON_VERSION_MAJOR": VERSION.split(".")[0],
